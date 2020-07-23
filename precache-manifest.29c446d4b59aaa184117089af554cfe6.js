43b0c3a23d74d64923029564691572bea3d4dd6d
NourMidan <58523548+NourMidan@users.noreply.github.com>
2020-07-23 17:12:38 +0200

    Updates

---
+++ precache-manifest.29c446d4b59aaa184117089af554cfe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fbda2ea681897805b5e05a5451b9e1",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "b395de516665fd541ea8",
+    "url": "/React-Calculator/static/css/main.3995e018.chunk.css"
+  },
+  {
+    "revision": "532017cbf2f95adc4023",
+    "url": "/React-Calculator/static/js/2.413d1334.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.413d1334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b395de516665fd541ea8",
+    "url": "/React-Calculator/static/js/main.fed6f225.chunk.js"
+  },
+  {
+    "revision": "31f1b382c927167c40c0",
+    "url": "/React-Calculator/static/js/runtime-main.b6e8a5a6.js"
+  }
+]);
